d3d9a686aa5416df3b72ea0c60b321dfcdad6029
mwanduka <42966698+mwanduka@users.noreply.github.com>
2018-11-20 16:39:43 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='12345'
-export SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://moringaschool:wonderwoman254@localhost/pitch
-export MAIL_USERNAME='mwandukastephen20@gmail.com'
-export MAIL_PASSWORD='totscaro'
-
-python3.6 manage.py server
